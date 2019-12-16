d0c1e6a60249e91c5742aa9f1ae5a5854368fb1d
Severin Klug <Severin.Klug@gmx.de>
2019-12-16 23:05:20 +0100

    chore(project): initialize project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ogc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'oGameCalculator';
+}
