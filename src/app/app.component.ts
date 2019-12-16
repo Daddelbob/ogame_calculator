aa8a4d1ed160551e2e136fc6164c2a27ee9ec0ad
Severin Klug <Severin.Klug@gmx.de>
2019-12-16 23:58:04 +0100

    chore(material) add material to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ogc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "ogc-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'oGameCalculator';
+  title = "oGameCalculator";
+
+  public universeSpeed: number = 7;
+  public metallMineLvl: number = 26;
+  public metallMineProd: number =
+    30 *
+    this.metallMineLvl *
+    Math.pow(1.1, this.metallMineLvl) *
+    this.universeSpeed;
+
+  constructor() {}
 }
