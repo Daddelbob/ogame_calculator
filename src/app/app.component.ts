8b603d9d7fac92b479b42d66f023f737001f95b5
Severin Klug <Severin.Klug@gmx.de>
2019-12-17 11:03:14 +0100

    feat(resources): add production table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,12 @@ export class AppComponent {
     this.universeSpeed;
 
   constructor() {}
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
 }
--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSliderModule,
+    MatSlideToggleModule
   ],
   exports: [
     MatSliderModule,
+    MatSlideToggleModule
   ]
 })
 export class CustomMaterialModule { }
